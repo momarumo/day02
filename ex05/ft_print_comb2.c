a3d7e987dba685c8477dba43899765ac3443052c
Mohau Marumo <momarumo@c5r6s7.wethinkcode.co.za>
2020-07-13 12:22:07 +0200

    mmj

---
+++ ex05/ft_print_comb2.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: momarumo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/12 08:45:10 by momarumo          #+#    #+#             */
+/*   Updated: 2020/07/13 12:19:59 by momarumo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+void	ft_putchar(char e)
+{
+	write(1 ,&e ,1);
+}
+void	ft_print_comb2(void)
+{
+	char a ='0';
+	char b ='0';
+	char c ='0';
+	char d ='0';
+
+	while( a <= '9')
+	{
+		while( b <= '9')
+		{
+			while( c <= '9')
+			{
+				while( d <= '9')
+				{
+					if (( a == '0' && b == '0') ||( c == '0' && d == '0'))
+					{
+					
+					}
+					else
+					{
+						ft_putchar(a);
+						ft_putchar(b);
+						ft_putchar(' ');
+						ft_putchar(c);
+						ft_putchar(d);
+						ft_putchar(',');
+					}
+					d++;
+					c='0';
+				}
+				c++;
+					b='0';
+			}
+			b++;
+			a='0';
+		}
+		a++;
+	}
+
+  
+}
+
+int	main()
+{
+	ft_print_comb2();
+	return 0;
+}
+	
